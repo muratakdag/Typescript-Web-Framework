52283a3c008ac2c9980e6dd951d21d82963eac34
Murat Akdağ <75887798+muratakdag@users.noreply.github.com>
2022-05-13 12:13:59 +0300

    Trigger Event Callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('laskjdf', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user);
+user.trigger('alskdjfalskjfdlksjdf');
