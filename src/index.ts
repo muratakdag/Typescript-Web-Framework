efcaa5a7a0882bd463f49f2791f1873a57a59537
Murat Akdağ <75887798+muratakdag@users.noreply.github.com>
2022-05-20 11:31:13 +0300

    REST Conventions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
-
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggered');
-});
-
-user.trigger('alskdjfalskjfdlksjdf');
+axios.get('http://localhost:3000/users/1');
