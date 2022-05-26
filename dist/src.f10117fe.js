629d2d9d700ccd1756f8e45ff9ba575e4fc6ccae
Murat Akdağ <75887798+muratakdag@users.noreply.github.com>
2022-05-26 09:12:55 +0300

    Save User Data

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2270,6 +2270,16 @@ var User = function () {
     });
   };
 
+  User.prototype.save = function () {
+    var id = this.get('id');
+
+    if (id) {
+      axios_1.default.put("http://localhost:3000/users/" + id, this.data);
+    } else {
+      axios_1.default.post('http://localhost:3000/users', this.data);
+    }
+  };
+
   return User;
 }();
 
@@ -2284,12 +2294,10 @@ Object.defineProperty(exports, "__esModule", {
 var User_1 = require("./models/User");
 
 var user = new User_1.User({
-  id: 1
+  name: 'new record',
+  age: 0
 });
-user.fetch();
-setTimeout(function () {
-  console.log(user);
-}, 4000);
+user.save();
 },{"./models/User":"src/models/User.ts"}],"../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -2318,7 +2326,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62178" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49735" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
